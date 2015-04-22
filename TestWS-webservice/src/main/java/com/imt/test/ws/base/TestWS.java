628cf3c950f8bac85825656a593ca14f61606a23
ahmadimt <ahmad.imt07@gmail.com>
2015-04-22 19:32:36 +0530

    Got WS Service

--- TestWS-webservice/src/main/java/com/imt/test/ws/base/TestWS.java
+++ TestWS-webservice/src/main/java/com/imt/test/ws/base/TestWS.java
@@ -4,7 +4,6 @@
 package com.imt.test.ws.base;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -12,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author imteyaza
@@ -20,12 +20,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Path("/home")
 public class TestWS {
-	
+
 	@Path("/index")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getHomePage(@Context HttpServletRequest request){
-		
+	public @ResponseBody Response getHomePage(@Context HttpServletRequest request) {
+
 		return Response.status(200).build();
 	}
 
